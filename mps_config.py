fec0c0809ac6d4fe60b1d99d284fc169b6c5119d
Matt Gibbs <mgibbs@gatech.edu>
2015-11-23 12:31:51 -0800

    Add a method to wipe the database clean.

--- mps_config.py
+++ mps_config.py
@@ -1,5 +1,6 @@
 import models
-from sqlalchemy import create_engine
+import contextlib
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 
 class MPSConfig:
@@ -7,3 +8,10 @@ class MPSConfig:
     self.engine = create_engine('sqlite:///mps_config.db')
     Session = sessionmaker(bind=self.engine)
     self.session = Session()
+  
+  def clear_all(self):
+    print("Clearing database...")
+    meta = models.Base.metadata
+    meta.bind = self.engine
+    meta.drop_all()
+    meta.create_all()
