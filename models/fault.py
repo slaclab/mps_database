d710edfa96d1168993ed04f2d4b7ced29abe1569
Matt Gibbs <mgibbs@gatech.edu>
2015-11-23 12:29:47 -0800

    Add convenience method for determining the fault's value, when given its input device states.

--- models/fault.py
+++ models/fault.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.orm import relationship, backref
 from models import Base
+from .fault_input import FaultInput
+from .device import Device
 
 class Fault(Base):
   __tablename__ = 'faults'
@@ -9,3 +11,11 @@ class Fault(Base):
   description = Column(String, nullable=True)
   states = relationship("FaultState", backref='fault')
   inputs = relationship("FaultInput", backref='fault')
+  
+  def fault_value(self, device_states):
+    fault_value = 0
+    for fault_input in self.inputs:
+      bit_length = len(fault_input.device.inputs)
+      input_value = device_states[fault_input.device_id]
+      fault_value = fault_value | (input_value << (bit_length*fault_input.bit_position))
+    return fault_value
