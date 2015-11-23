c8caa3a784712a1cf2db16ed5fdb80223e25d83b
Matt Gibbs <mgibbs@gatech.edu>
2015-11-23 12:29:00 -0800

    Add convenience methods for adding allowed beam classes.

--- models/fault_state.py
+++ models/fault_state.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship, backref
 from models import Base
+from .allowed_class import AllowedClass
 
 class FaultState(Base):
   __tablename__ = 'fault_states'
@@ -9,3 +10,17 @@ class FaultState(Base):
   name = Column(String, nullable=False)
   fault_id = Column(Integer, ForeignKey('faults.id'), nullable=False)
   allowed_classes = relationship("AllowedClass", backref='fault_state')
+  
+  def add_allowed_class(self, beam_class, mitigation_device):
+    ac = AllowedClass()
+    ac.beam_class = beam_class
+    ac.mitigation_device = mitigation_device
+    self.allowed_classes.append(ac)
+    return ac
+  
+  def add_allowed_classes(self, beam_classes, mitigation_device):
+    acs = []
+    for c in beam_classes:
+      acs.append(self.add_allowed_class(c, mitigation_device))
+    return acs
+      
