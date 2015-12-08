700ae4ff492bafb743eeb823c92b6174a82e800f
Matt Gibbs <mgibbs@gatech.edu>
2015-12-08 09:12:04 -0800

    Add some validations to the crate model.

--- models/crate.py
+++ models/crate.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, validates
 from models import Base
 
 class Crate(Base):
@@ -8,3 +8,33 @@ class Crate(Base):
   number = Column(Integer, unique=True, nullable=False)
   num_slots = Column(Integer, nullable=False)
   cards = relationship("ApplicationCard", backref='crate')
+  
+  @validates('cards')
+  def validate_card(self, key, card):
+    #Ensure the crate isn't full
+    if len(self.cards) < self.num_slots:
+      raise ValueError("Crate cannot have more cards than num_slots.")
+    
+    #Ensure the slot exists
+    if card.slot_number <= self.num_slots:
+      raise ValueError("Card cannot use slot_number > num_slots.")
+    
+    #Ensure the slot isn't taken
+    if card.slot_number not in [c.slot_number for c in self.cards]:
+      raise ValueError("Slot is already taken by another card.")
+    return cards
+  
+  @validates('num_slots')
+  def validate_num_slots(self, key, new_num_slots):
+    #Ensure there are enough slots to fit all the installed cards.
+    if new_num_slots >= len(self.cards):
+      raise ValueError("num_slots must be >= than the number of cards in this crate.")
+    
+    #Ensure the installed cards are still in valid slots
+    for c in self.cards:
+      if c.slot_number <= new_num_slots:
+        raise ValueError("new value for num_slots would invalidate a currently installed card.")
+    
+    return new_num_slots
+    
+    
