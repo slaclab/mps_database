05f0665d998ce82b4e61c03c784b9b2d4cd69cd2
Matt Gibbs <mgibbs@gatech.edu>
2015-12-15 23:41:51 -0800

    Add some validations to application card and card type.

--- models/application_card.py
+++ models/application_card.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, ForeignKey
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship, backref, validates
 from models import Base
 
 class ApplicationCard(Base):
@@ -10,3 +10,20 @@ class ApplicationCard(Base):
   crate_id = Column(Integer, ForeignKey('crates.id'), nullable=False)
   type_id = Column(Integer, ForeignKey('application_card_types.id'), nullable=False)
   channels = relationship("Channel", backref='card')
+  
+  @validates('channels')
+  def validate_channel(self, key, new_channel):
+    if len(self.channels)+1 > self.type.channel_count:
+      raise ValueError("Number of channels on this card cannot exceed the card type's channel count ({count})".format(count=self.type.channel_count))
+    
+    if new_channel.number >= self.type.channel_count:
+      raise ValueError("For this card's type, channel number must be < {count}".format(count=self.type.channel_count))
+    
+    if new_channel.number < 0:
+      raise ValueError("Channel number must be positive.")
+    
+    #Ensure the channel isn't taken
+    if new_channel.number in [c.number for c in self.channels]:
+      raise ValueError("Channel number {num} is already taken by an existing channel.".format(num=new_channel.number))
+    
+    return new_channel
--- models/application_card_type.py
+++ models/application_card_type.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, validates
+from sqlalchemy.sql import func
 from models import Base
 class ApplicationCardType(Base):
   __tablename__ = 'application_card_types'
@@ -9,3 +10,26 @@ class ApplicationCardType(Base):
   channel_count = Column(Integer, nullable=False)
   channel_size = Column(Integer, nullable=False)
   cards = relationship("ApplicationCard", backref='type')
+  
+  @validates('cards')
+  def validate_cards(self, key, card):
+    if self.channel_count < len(card.channels):
+      raise ValueError("Card cannot have a type with channel_count < the number of channels connected to the card.")
+    return card
+  
+  @validates('channel_count')
+  def validate_channel_count(self, key, new_count):
+    if new_count < 1:
+      raise ValueError("Type must have a channel count >= 1.")
+    
+    #This is slow and bad.
+    for c in self.cards:
+      if len(c.channels) > new_count:
+        raise ValueError("New number of channels would not be compatible with existing card(s).")
+    
+    return new_count
+  
+  @validates('channel_size')
+  def validate_channel_size(self, key, new_size):
+    if new_size < 1:
+      raise ValueError("Channel size must be >= 1.")
