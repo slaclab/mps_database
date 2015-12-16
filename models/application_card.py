f0940823fed08008b3d75b7333d3cf25706c74e6
Matt Gibbs <mgibbs@gatech.edu>
2015-12-16 09:34:12 -0800

    Improve application card validations.

--- models/application_card.py
+++ models/application_card.py
@@ -13,10 +13,10 @@ class ApplicationCard(Base):
   
   @validates('channels')
   def validate_channel(self, key, new_channel):
-    if len(self.channels)+1 > self.type.channel_count:
+    if self.type and len(self.channels)+1 > self.type.channel_count:
       raise ValueError("Number of channels on this card cannot exceed the card type's channel count ({count})".format(count=self.type.channel_count))
     
-    if new_channel.number >= self.type.channel_count:
+    if self.type and new_channel.number >= self.type.channel_count:
       raise ValueError("For this card's type, channel number must be < {count}".format(count=self.type.channel_count))
     
     if new_channel.number < 0:
@@ -27,3 +27,13 @@ class ApplicationCard(Base):
       raise ValueError("Channel number {num} is already taken by an existing channel.".format(num=new_channel.number))
     
     return new_channel
+  
+  @validates('slot_number')
+  def validate_slot_number(self, key, new_slot):
+    if self.crate and new_slot > self.crate.num_slots:
+      raise ValueError("Slot number must be <= the number of slots in the crate ({count})".format(count=self.crate.num_slots))
+    
+    if self.crate and new_slot in [c.number for c in self.crate.cards]:
+      raise ValueError("This slot is already taken by another card in the crate.")
+    
+    return new_slots
