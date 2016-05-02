de36cac93a8f2b996d93eff4d9e0c7b319e35696
Matt Gibbs <mgibbs@gatech.edu>
2016-05-02 16:12:08 -0700

    Add a "units" string to the analog device type table.  Just for convenience.

--- models/analog_device_type.py
+++ models/analog_device_type.py
@@ -7,4 +7,5 @@ class AnalogDeviceType(Base):
   id = Column(Integer, primary_key=True)
   name = Column(String, nullable=False, unique=True)
   threshold_value_map_id = Column(Integer, ForeignKey('threshold_value_maps.id'), nullable=False)
+  units = Column(String, nullable=False)
   analog_devices = relationship("AnalogDevice", backref='analog_device_type')
