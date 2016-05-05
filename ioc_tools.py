2e1b1db0149d9aa9fe74b51e338cdfaa57bd1beb
Matt Gibbs <mgibbs@gatech.edu>
2016-05-05 13:32:31 -0700

    Add a method to serialize the database into YAML.

--- ioc_tools.py
+++ ioc_tools.py
@@ -1,38 +1,51 @@
 from mps_config import MPSConfig
 import models
-import json
-
-def generate_json_for_link_node_ioc(link_node_number):
-  mps = MPSConfig()
-  session = mps.session
-  
-  crate = session.query(models.Crate).filter(models.Crate.number == link_node_number).one()
-  crate_dict = {"name": "LinkNode{number}".format(number=crate.number)}
-  apps = []
-  for card in crate.cards:
-    card_dict = {}
-    card_dict["name"] = "{type}".format(type=card.type.name)
-    card_dict["ip"] = ip_address_for_card(card)
-    card_dict["port"] = 123456
+import yaml
+from yaml import MappingNode, ScalarNode
+from sqlalchemy import Column, Integer, Float, String, Boolean
+def dump_db_to_yaml(mps_config, filename):
+  session = mps_config.session
+  f = file(filename, 'w')
+  yaml.add_multi_representer(models.Base, model_representer)
+  model_classes = [models.Crate,
+                  models.ApplicationType,
+                  models.ApplicationCard,
+                  models.DigitalChannel,
+                  models.AnalogChannel,
+                  models.DeviceType,
+                  models.DeviceState,
+                  models.DigitalDevice,
+                  models.DeviceInput,
+                  models.Fault,
+                  models.FaultInput,
+                  models.DigitalFaultState,
+                  models.ThresholdValueMap,
+                  models.ThresholdValue,
+                  models.AnalogDeviceType,
+                  models.AnalogDevice,
+                  models.ThresholdFault,
+                  models.ThresholdFaultState,
+                  models.MitigationDevice,
+                  models.BeamClass,
+                  models.AllowedClass]
+  for model_class in model_classes:
+    collection = session.query(model_class).order_by(model_class.id).all()
     
-    #Now we'll add register addresses for each threshold map.
-    analog_devices = session.query(models.AnalogDevice).join(models.AnalogDevice.channel).filter(models.Channel.card_id == card.id).order_by(models.Channel.number).all()
-    registers = []
-    address = 0x0C000000
-    for analog_device in analog_devices:
-      register_dict = {}
-      nelm = 256
-      value_size = 4
-      register_dict["name"] = "_".join(analog_device.name.split())
-      register_dict["type"] = "Threshold"
-      register_dict["nelm"] = nelm
-      register_dict["addr"] = hex(address)
-      address += nelm*value_size
-      registers.append(register_dict)
-    card_dict["reg"] = registers
-    apps.append(card_dict)
-  crate_dict["apps"] = apps
-  return json.dumps(crate_dict, sort_keys=True, indent=2, separators=(',', ': '))
-      
-def ip_address_for_card(card):
-  return "192.168.{shelf_num}.{slot_num}".format(shelf_num=card.crate.shelf_number, slot_num=card.slot_number)
+    yaml.dump({model_class.__name__: collection}, f, explicit_start=True)  
+  f.close()
+  
+def model_representer(dumper, obj):
+  node = MappingNode(tag=u'tag:yaml.org,2002:map', value=[], flow_style=False)
+  for column in obj.__table__.columns:
+    if getattr(obj, column.name) == None:
+      continue
+    print("Generating node for col_name = {col_name}, type = {col_type}, val = {val}".format(col_name=column.name, col_type=column.type, val=str(getattr(obj, column.name))))
+    if isinstance(column.type, String):
+      node.value.append((ScalarNode(tag=u'tag:yaml.org,2002:str', value=column.name), (ScalarNode(tag=u'tag:yaml.org,2002:str', value=getattr(obj, column.name), style='"'))))
+    elif isinstance(column.type, Integer):
+      node.value.append((ScalarNode(tag=u'tag:yaml.org,2002:str', value=column.name), (ScalarNode(tag=u'tag:yaml.org,2002:int', value=str(getattr(obj, column.name)), style=''))))
+    elif isinstance(column.type, Float):
+      node.value.append((ScalarNode(tag=u'tag:yaml.org,2002:str', value=column.name), (ScalarNode(tag=u'tag:yaml.org,2002:float', value=str(getattr(obj, column.name)), style=''))))
+    elif isinstance(column.type, Boolean):
+      node.value.append((ScalarNode(tag=u'tag:yaml.org,2002:str', value=column.name), (ScalarNode(tag=u'tag:yaml.org,2002:bool', value=str(getattr(obj, column.name)), style=''))))
+  return node
