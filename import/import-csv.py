cbbc13214717c1ccc481ac8759fa8bb48c3388d3
Luciano Piccoli <lpiccoli@slac.stanford.edu>
2018-03-01 10:43:58 -0800

    Addition of drawing number to device table import-csv.py script needs work to import devices, and generate the proper faults and mitigation. Database changes are needed for post-EIC database

--- import/import-csv.py
+++ import/import-csv.py
@@ -43,7 +43,7 @@ class DatabaseImporter:
         self.session.add(crate)
       
     self.session.commit()
-    f.close
+    f.close()
 
 
   def add_app_types(self, file_name):
@@ -75,7 +75,7 @@ class DatabaseImporter:
         self.session.add(app_type)
       
     self.session.commit()
-    f.close
+    f.close()
 
   def add_cards(self, file_name):
     f = open(file_name)
@@ -114,31 +114,108 @@ class DatabaseImporter:
                                           global_id=int(app_card_info['global_id']),
                                           description="EIC Digital Input/Output")
 
-#        app_card = models.ApplicationCard(name=app_card_info['name'],
-#                                          number=app_card_info['number'],
-#                                          area=app_card_info['area'],
-#                                          location=app_card_info['location'],
-#                                          slot_number=int(app_card_info['slot']),
-#                                          type=app_card_type,
-#                                          crate=app_crate,
-#                                          amc=int(app_card_info['amc']),
-#                                          global_id=app_card_info['global_id'],
-#                                          description=app_card_info['description'])
-#        app_card.show()
-
-#        crate.cards.append(app_card)
-#        self.session.add(app_card)
         app_crate.cards.append(app_card)
         self.session.add(app_card)
 
-#    self.session.commit()
-    f.close
+    self.session.commit()
+    f.close()
+
+  def add_device_types(self, file_name):
+    f = open(file_name)
+
+    line = f.readline().strip()
+    fields=[]
+    for field in line.split(','):
+      fields.append(str(field).lower())
+
+    while line:
+      device_type_info={}
+      line = f.readline().strip()
+
+      if line:
+        field_index = 0
+        for property in line.split(','):
+          device_type_info[fields[field_index]]=property
+          field_index = field_index + 1
+
+        device_type = models.DeviceType(name=device_type_info['name'],
+                                        description=device_type_info['description'],
+                                        num_integrators=int(device_type_info['num_integrators']))
+
+        self.session.add(device_type)
+
+    self.session.commit()
+    f.close()
+
+# linac = models.BeamDestination(name="LINAC", description="Linac destination", destination_mask=0x01)
+  def add_beam_destinations(self, file_name):
+    f = open(file_name)
+
+    line = f.readline().strip()
+    fields=[]
+    for field in line.split(','):
+      fields.append(str(field).lower())
+
+    while line:
+      destination_info={}
+      line = f.readline().strip()
+
+      if line:
+        field_index = 0
+        for property in line.split(','):
+          destination_info[fields[field_index]]=property
+          field_index = field_index + 1
+
+        beam_destination = models.BeamDestination(name=destination_info['name'],
+                                                  description=destination_info['description'],
+                                                  destination_mask=int(destination_info['destination_mask']))
+
+        self.session.add(beam_destination)
+
+    self.session.commit()
+    f.close()
+
+#class_0 = models.BeamClass(number=0,name="Power Class 0",description="No Beam",
+#                              integration_window=10, total_charge=100000, min_period=1)
+  def add_beam_classes(self, file_name):
+    f = open(file_name)
+
+    line = f.readline().strip()
+    fields=[]
+    for field in line.split(','):
+      fields.append(str(field).lower())
+
+    while line:
+      class_info={}
+      line = f.readline().strip()
+
+      if line:
+        field_index = 0
+        for property in line.split(','):
+          class_info[fields[field_index]]=property
+          field_index = field_index + 1
+
+        beam_class = models.BeamClass(number=class_info['number'],
+                                      name=class_info['name'],
+                                      description=class_info['description'],
+                                      integration_window=int(class_info['integration_window']),
+                                      total_charge=int(class_info['total_charge']),
+                                      min_period=int(class_info['min_period']))
+
+        self.session.add(beam_class)
+
+    self.session.commit()
+    f.close()
+
 
 importer = DatabaseImporter("mps_config_imported.db")
 
 importer.add_crates('import/Crates.csv')
 importer.add_app_types('import/AppTypes.csv')
 importer.add_cards('import/AppCards.csv')
+importer.add_device_types('import/DeviceTypes.csv')
+importer.add_beam_destinations('import/BeamDestinations.csv')
+importer.add_beam_classes('import/BeamClasses.csv')
 
 #link_node_card = models.ApplicationCard(name="EIC Digital Card", number=100, area="GUNB",
 #                                        location="MP10", type=eic_digital_app, slot_number=2, amc=2, #amc=2 -> RTM
