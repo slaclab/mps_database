6959a344593a7a21295596f57ff9bc15ce6ccfa1
Luciano Piccoli <lpiccoli@slac.stanford.edu>
2018-03-19 15:21:50 -0700

    moved env var setting to the end

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-export LD_LIBRARY_PATH=$PACKAGE_TOP/python2.7.9/linux-x86_64/lib:$LD_LIBRARY_PATH
-export PATH=$PACKAGE_TOP/python2.7.9/linux-x86_64/bin:$PATH
 
 if [ $# == 1 ]; then
   if [ "$1" == "install" ]; then
@@ -14,3 +12,6 @@ else
     echo "ERROR: use '. ./setup.sh install' command first"
   fi
 fi
+
+export LD_LIBRARY_PATH=$PACKAGE_TOP/python/python2.7.9/linux-x86_64/lib:$LD_LIBRARY_PATH
+export PATH=$PACKAGE_TOP/python/python2.7.9/linux-x86_64/bin:$PATH
