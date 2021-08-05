debbb26c44431bf97694229df61c72a179d1312b
King <lking1106@gmail.com>
2021-08-04 17:20:20 -0700

    add single entry to history db

---
+++ mps_database/history/tools/create_history.py
@@ -0,0 +1,33 @@
+from mps_database import mps_config, models
+from mps_database.history.models import fault_history, input_history
+from mps_database.models import Base
+
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship, backref
+
+
+
+def main():
+    delete_history_db()
+    create_history_db()
+    return
+
+
+def create_history_db():
+    tables_to_build = {fault_history.FaultHistory.__table__, input_history.InputHistory.__table__}
+    config_engine = mps_config.MPSConfig(db_file="mps_gun_history.db", db_name="config").last_engine
+    Base.metadata.create_all(config_engine, tables=tables_to_build)
+    #build_fault_table("test").__table__.create(bind = config_engine.last_engine)
+    return
+
+def delete_history_db():
+    #Add function to delete all tables/rows
+    meta = models.Base.metadata
+    meta.bind = mps_config.MPSConfig(db_file="mps_gun_history.db", db_name="config").last_engine
+    meta.drop_all(tables=[fault_history.FaultHistory.__table__, input_history.InputHistory.__table__])
+    return
+
+if __name__ == "__main__":
+    main()
+
+
--- mps_database/history/tools/history_tools.py
+++ mps_database/history/tools/history_tools.py
@@ -1,12 +1,32 @@
 from mps_database.mps_config import MPSConfig, models
-from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
+from mps_database.history.models import fault_history, input_history
+from sqlalchemy import insert
+
 from sqlalchemy.orm import relationship, backref
 
+"""
+class HistorySession():
+    def __init__(self):
+
+        return
+"""
+#use for testing
+def main():
+    history_class = connect_hist_db()
+    print("\n", history_class.session)
+
+    fault_id = 1
+    add_fault(history_class, fault_id)
+    faults = [22, 23, 24]
+    add_faults(history_class, faults)
+    return
 
 
-# this probably already exists, see if I can establish a connection to the config db
+def connect_hist_db():
+    conf = MPSConfig(db_name="history", db_file='mps_gun_history.db')
+    return conf
 
-def connect_db():
+def connect_conf_db():
     # gun, runtime dbs hardcoded for now
     #TODO: add cli args later
     conf = MPSConfig(db_name="config", db_file='mps_config_imported.db')
@@ -15,14 +35,28 @@ def connect_db():
 #TODO: is imported the default name now? Or is that just for testing?
 
 def query_beamclass():
-    conf_session = connect_db().session()
+    conf_session = connect_conf_db().session()
     print(conf_session)
     print(conf_session.query(models.BeamClass.name).all())
     return
 
-def add_log():
+def add_fault(history_conn, fault_id):
+    fault_insert = fault_history.FaultHistory.__table__.insert().values(id=fault_id, timestamp='time')
+    history_conn.last_engine.execute(fault_insert)
+    print("\n", history_conn.session)
+    history_conn.session.commit()
+    return
+
+def add_faults(history_conn, fault_ids):
+    faults_info = []
+    for fid in fault_ids:
+        faults_info += ({'id': fid, 'timestamp':'time'})
+    history_conn.last_engine.execute(fault_history.FaultHistory.__table__.insert(), faults_info)
+    history_conn.session.commit()
     return
     
+def get_entry():
+    return
 
 if __name__ == "__main__":
-    query_beamclass()
+    main()
