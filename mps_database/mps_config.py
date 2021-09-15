c8a77103bd0c2498e12721e4e433021abbbfd430
King <lking1106@gmail.com>
2021-09-14 18:10:43 -0700

    post move history files to new repo mps_history

--- mps_database/mps_config.py
+++ mps_database/mps_config.py
@@ -15,11 +15,18 @@ class MPSConfig:
   Properties:
   References:
   """
-  def __init__(self, config_file_name='mps_gun_config.db', runtime_file_name='mps_gun_runtime.db', history_file_name='mps_gun_history.db', db_name=None, db_file=None, debug=False):
+  def __init__(self, config_file_name='mps_gun_config.db', runtime_file_name='mps_gun_runtime.db', history_file_name='mps_gun_history.db', db_name=None, db_file=None, file_path=None, debug=False):
     self.db_name = db_name
     self.last_engine = None
     
     current_dbs = ["config", "runtime", "history"]
+
+    #TODO: create real file path defaults - defaults file?
+    DEV_RUNTIME_PATH = "/u1/lcls/physics/mps_manager"
+    DEV_HISTORY_PATH = "/u1/lcls/physics/mps_history"
+    DEV_CONFIG_PATH = "$PHYSICS_TOP/mps_configuration/current"
+    lcls_dev = False
+
     
     # One specific connection is requested
     if self.db_name is not None:
@@ -27,33 +34,26 @@ class MPSConfig:
         # If no filename provided, use default config
         if not db_file:
             db_file = config_file_name
-        self.create_db_session(db_file, debug)
+        self.create_db_session(db_file, debug, file_path=file_path)
         return
-    # Create all sessions
-    self.session = self.create_db_session(config_file_name, debug)
-    self.runtime_session = self.create_db_session(runtime_file_name, debug)
-    self.history_session = self.create_db_session(history_file_name, debug)
 
-    """
-    self.engine = create_engine("sqlite:///{filename}".format(filename=filename), echo=debug)
-    self.Session = sessionmaker(bind=self.engine)
-    self.session = self.Session()
-    #TODO: the way these sessions work defeat the point of a session maker. Can call this session creator anywhere and they can act as one time sessions
-  
-    self.runtime_engine = create_engine("sqlite:///{filename}".format(filename=runtime_file_name), echo=debug)
-    self.Runtime_Session = sessionmaker(bind=self.runtime_engine)
-    self.runtime_session = self.Runtime_Session()
+    if lcls_dev:
+      # Create all sessions
+      self.session = self.create_db_session(config_file_name, debug, file_path=DEV_CONFIG_PATH)
+      self.runtime_session = self.create_db_session(runtime_file_name, debug, file_path=DEV_RUNTIME_PATH)
+      self.history_session = self.create_db_session(history_file_name, debug, file_path=DEV_HISTORY_PATH)
+    else:
+      # Create all sessions
+      self.session = self.create_db_session(config_file_name, debug)
+      self.runtime_session = self.create_db_session(runtime_file_name, debug)
+      self.history_session = self.create_db_session(history_file_name, debug)
 
-    self.history_engine = create_engine("sqlite:///{filename}".format(filename=history_file_name), echo=debug)
-    self.History_Session = sessionmaker(bind=self.history_engine)
-    self.history_session = self.History_Session()
-    """
-
-  def create_db_session(self, filename, debug):
+  def create_db_session(self, filename, debug, file_path=None):
     # Make a path to the directory MPSConfig is in, assume db is in there as well
-    db_path = os.path.join(os.path.dirname(__file__), filename)
-
-    engine = create_engine("sqlite:///{path_to_filename}".format(path_to_filename=db_path), echo=debug)
+    if not file_path:
+      file_path = os.path.join(os.path.dirname(__file__), filename)
+    print("DB File Path: ", file_path)
+    engine = create_engine("sqlite:///{path_to_filename}".format(path_to_filename=file_path), echo=debug)
     self.last_engine = engine
     
     Session = sessionmaker(bind=engine)
