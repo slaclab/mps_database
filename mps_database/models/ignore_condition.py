06e02663581323073e6a8481d8e91b57c9b4a3dc
Jeremy Mock <jeremy.a.mock@gmail.com>
2022-01-06 08:50:03 -0800

    Change analog_device_id and analogDevice link to device_id and device table link

--- mps_database/models/ignore_condition.py
+++ mps_database/models/ignore_condition.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship, backref
 from mps_database.models import Base
-from .allowed_class import AllowedClass
+#from .allowed_class import AllowedClass
 
 class IgnoreCondition(Base):
   """
@@ -14,10 +14,10 @@ class IgnoreCondition(Base):
     condition_id: if this condition is true than the fault_state or the analog_device
                   have faults ignored.
     fault_state_id: fault_state to be ignored (only used by EIC)
-    analog_device_id: analog_device to be ignored.
+    device_id: analog_device to be ignored.
   """
   __tablename__ = 'ignore_conditions'
   id = Column(Integer, primary_key=True)
   condition_id = Column(Integer, ForeignKey('conditions.id'), nullable=False)
   fault_state_id = Column(Integer, ForeignKey('fault_states.id'))
-  analog_device_id = Column(Integer, ForeignKey('analog_devices.id'), nullable=False)
+  device_id = Column(Integer, ForeignKey('devices.id'), nullable=False)
