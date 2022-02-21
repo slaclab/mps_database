bdf0cb7b4a52112c61aedf36089d30bc4289ebf1
Jeremy Mock <jeremy.a.mock@gmail.com>
2022-02-21 14:57:59 -0800

    Add link node group to model schema

---
+++ mps_database/models/groups.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, ForeignKey, String
+from sqlalchemy.orm import relationship, backref, validates
+from mps_database.models import Base
+
+class LinkNodeGroup(Base):
+  """
+  LinkNodeGroup class (link_nodes table)
+
+  Defines a Link Node (SIOC), which has a one-to-one mapping with crates. 
+  Every crate has one LN. The LN also contains information about all
+  the application cards configured within the crate.
+
+  Properties:
+   number: group number
+   
+  References:
+   link_nodes: specifies the crate that contains this link node
+  """
+  __tablename__ = 'link_node_groups'
+  id = Column(Integer, primary_key=True)
+  number = Column(Integer, nullable=False)
+  link_nodes = relationship("LinkNode", order_by="LinkNode.lcls1_id", backref='link_node_groups')
+  central_node1 = Column(Integer,nullable=False)
+  central_node2 = Column(String,nullable=False)
