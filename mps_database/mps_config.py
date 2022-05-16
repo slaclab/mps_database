c6ce77bf41ba62249faad3126b1de60f7531f9f8
Laura King <lking1106@gmail.com>
2022-05-16 12:20:06 -0700

    Database File Chooser (#29)

--- mps_database/mps_config.py
+++ mps_database/mps_config.py
@@ -1,13 +1,15 @@
 
 from mps_database import models
 from mps_database import runtime 
-import contextlib
+import contextlib, glob
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 
 class MPSConfig:
   #def __init__(self, filename='mps_gun_config.db', runtime_file_name='mps_gun_runtime.db', debug=False):
-  def __init__(self, filename='mps_gun_config.db', debug=False):
+  def __init__(self, filename=None, debug=False):
+    if not filename:
+      filename = self.find_dbfile()
     self.engine = create_engine("sqlite:///{filename}".format(filename=filename), echo=debug)
     self.Session = sessionmaker(bind=self.engine)
     self.session = self.Session()
@@ -18,6 +20,28 @@ class MPSConfig:
     meta.bind = self.engine
     meta.drop_all()
     meta.create_all()
+  
+  def find_dbfile(self):
+    """
+    Tries to find the most recent db configuration to use.
+    Default db name of mps_gun_config.db
+    """
+    afs_current_path = "/afs/slac/g/lcls/physics/mps_configuration/current/"
+    local_path = "/usr/local/lcls/physics/mps_configuration/current"
+    db_naming = "mps_config-[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]*.db"
+
+    try:
+    #Add nums to exclude -cn- dbs
+      dbs = sorted(glob.glob(local_path+db_naming))
+      if not dbs:
+        dbs = sorted(glob.glob(afs_current_path+db_naming))
+      if not dbs:
+        raise
+      filename = dbs[-1]
+    except:
+      filename = "mps_gun_config.db"
+    print("Choosing ", filename, " as configuration db.")
+    return filename
 
   def find_device_type(self,session,typ,analog=False):
     if analog:
