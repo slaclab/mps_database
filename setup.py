88651e5d9c4e2816ae2aa31ee97310aafe3f7d0a
Jeremy Mock <jeremy.a.mock@gmail.com>
2022-06-29 10:06:25 -0700

    Change requirements for cheetah to cheetah3

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ with open('./requirements.txt') as f:
     
 setup(
     name='mps_database',
-    version='0.2.0',
-    author='Laura King',
-    author_email='lking@slac.stanford.edu',
+    version='1.0.0',
+    author='Jeremy Mock',
+    author_email='jmock@slac.stanford.edu',
     packages=find_packages(include=['mps_database'], exclude=[]),
     url='https://github.com/slaclab/mps_database',
     license='MIT',
     description='Various tools and scripts for the mps database.',
     long_description=open('README.md').read(),
+    install_requires=requirements
 )
