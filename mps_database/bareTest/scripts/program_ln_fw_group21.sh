892030a9f1876be1532d8a1f90abed8f7821aacf
Jeremy Mock <jeremy.a.mock@gmail.com>
2022-11-14 15:37:56 -0800

    Delete more files

--- mps_database/bareTest/scripts/program_ln_fw_group21.sh
+++
@@ -1,4 +0,0 @@
-$PACKAGE_TOP/cpsw/utils/ProgramFPGA/current/ProgramFPGA.bash -n 2 -c cpu-unds-sp02 -s shm-unds-sp02-2 -m $1
-$PACKAGE_TOP/cpsw/utils/ProgramFPGA/current/ProgramFPGA.bash -n 2 -c cpu-fees-sp01 -s shm-fees-sp01-1 -m $1
-$PACKAGE_TOP/cpsw/utils/ProgramFPGA/current/ProgramFPGA.bash -n 2 -c cpu-fees-sp01 -s shm-fees-sp01-1 -m $1
-$PACKAGE_TOP/cpsw/utils/ProgramFPGA/current/ProgramFPGA.bash -n 2 -c cpu-fees-sp01 -s shm-fees-sp01-1 -m $1
--- mps_database/bareTest/scripts/program_ln_fw_group8.sh
+++
@@ -1,4 +0,0 @@
-$PACKAGE_TOP/cpsw/utils/ProgramFPGA/current/ProgramFPGA.bash -n 2 -c cpu-bpn13-sp01 -s shm-bpn13-sp01-1 -m $1
-$PACKAGE_TOP/cpsw/utils/ProgramFPGA/current/ProgramFPGA.bash -n 2 -c cpu-bpn14-sp01 -s shm-bpn14-sp01-1 -m $1
-$PACKAGE_TOP/cpsw/utils/ProgramFPGA/current/ProgramFPGA.bash -n 2 -c cpu-bpn15-sp01 -s shm-bpn15-sp01-1 -m $1
-$PACKAGE_TOP/cpsw/utils/ProgramFPGA/current/ProgramFPGA.bash -n 2 -c cpu-bpn16-sp01 -s shm-bpn16-sp01-1 -m $1
