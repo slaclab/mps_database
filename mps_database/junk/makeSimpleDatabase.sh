647290e4a7536db2e6b727c473654c2a5c76e8fb
Jeremy Mock <jmock@dev-srv09.slac.stanford.edu>
2026-05-11 10:39:42 -0700

    Remove unnecessary files

--- mps_database/junk/makeSimpleDatabase.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-database/add_data.py -v -n --db really_simple.db --file even_simpler/nodes.json
-database/add_data.py -v --db really_simple.db --file even_simpler/ignore.json
-database/add_data.py -v --db really_simple.db --file even_simpler/logic.json
-
-
